5239aa891b5bb2843f662a0d73905c3688d99190
Ba Samson Lam <ba-samson.duong@polymtl.ca>
2016-04-02 21:15:07 -0400

    tabulation mode

---
+++ interfacetest/interfacetest/main.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace interfacetest
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.ActuatorBox = new System.Windows.Forms.GroupBox();
+            this.ReclineBox = new System.Windows.Forms.GroupBox();
+            this.LegsBox = new System.Windows.Forms.GroupBox();
+            this.Main_DeviceBox = new System.Windows.Forms.GroupBox();
+            this.ElevateBox = new System.Windows.Forms.GroupBox();
+            this.TiltBox = new System.Windows.Forms.GroupBox();
+            this.MiscallenousBox = new System.Windows.Forms.GroupBox();
+            this.sauvegardebtnpage1 = new System.Windows.Forms.Button();
+            this.inputDeviceBox = new System.Windows.Forms.GroupBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.fowardbox = new System.Windows.Forms.GroupBox();
+            this.reversebox = new System.Windows.Forms.GroupBox();
+            this.turnbox = new System.Windows.Forms.GroupBox();
+            this.joystick = new System.Windows.Forms.GroupBox();
+            this.speedbox = new System.Windows.Forms.GroupBox();
+            this.sauvegardebtnpage2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(842, 596);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.ActuatorBox);
+            this.tabPage1.Controls.Add(this.ReclineBox);
+            this.tabPage1.Controls.Add(this.LegsBox);
+            this.tabPage1.Controls.Add(this.Main_DeviceBox);
+            this.tabPage1.Controls.Add(this.ElevateBox);
+            this.tabPage1.Controls.Add(this.TiltBox);
+            this.tabPage1.Controls.Add(this.MiscallenousBox);
+            this.tabPage1.Controls.Add(this.sauvegardebtnpage1);
+            this.tabPage1.Controls.Add(this.inputDeviceBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(834, 570);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Configuration";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // ActuatorBox
+            // 
+            this.ActuatorBox.Location = new System.Drawing.Point(603, 12);
+            this.ActuatorBox.Name = "ActuatorBox";
+            this.ActuatorBox.Size = new System.Drawing.Size(251, 198);
+            this.ActuatorBox.TabIndex = 9;
+            this.ActuatorBox.TabStop = false;
+            this.ActuatorBox.Text = "Actuator Control Device";
+            // 
+            // ReclineBox
+            // 
+            this.ReclineBox.Location = new System.Drawing.Point(447, 12);
+            this.ReclineBox.Name = "ReclineBox";
+            this.ReclineBox.Size = new System.Drawing.Size(140, 109);
+            this.ReclineBox.TabIndex = 11;
+            this.ReclineBox.TabStop = false;
+            this.ReclineBox.Text = "Recline";
+            // 
+            // LegsBox
+            // 
+            this.LegsBox.Location = new System.Drawing.Point(288, 159);
+            this.LegsBox.Name = "LegsBox";
+            this.LegsBox.Size = new System.Drawing.Size(140, 109);
+            this.LegsBox.TabIndex = 10;
+            this.LegsBox.TabStop = false;
+            this.LegsBox.Text = "Legs";
+            // 
+            // Main_DeviceBox
+            // 
+            this.Main_DeviceBox.Location = new System.Drawing.Point(59, 12);
+            this.Main_DeviceBox.Name = "Main_DeviceBox";
+            this.Main_DeviceBox.Size = new System.Drawing.Size(206, 109);
+            this.Main_DeviceBox.TabIndex = 9;
+            this.Main_DeviceBox.TabStop = false;
+            this.Main_DeviceBox.Text = "Main Device";
+            // 
+            // ElevateBox
+            // 
+            this.ElevateBox.Location = new System.Drawing.Point(447, 159);
+            this.ElevateBox.Name = "ElevateBox";
+            this.ElevateBox.Size = new System.Drawing.Size(140, 109);
+            this.ElevateBox.TabIndex = 12;
+            this.ElevateBox.TabStop = false;
+            this.ElevateBox.Text = "Elevate";
+            // 
+            // TiltBox
+            // 
+            this.TiltBox.Location = new System.Drawing.Point(288, 12);
+            this.TiltBox.Name = "TiltBox";
+            this.TiltBox.Size = new System.Drawing.Size(140, 109);
+            this.TiltBox.TabIndex = 9;
+            this.TiltBox.TabStop = false;
+            this.TiltBox.Text = "Tilt";
+            // 
+            // MiscallenousBox
+            // 
+            this.MiscallenousBox.Location = new System.Drawing.Point(603, 230);
+            this.MiscallenousBox.Name = "MiscallenousBox";
+            this.MiscallenousBox.Size = new System.Drawing.Size(251, 267);
+            this.MiscallenousBox.TabIndex = 9;
+            this.MiscallenousBox.TabStop = false;
+            this.MiscallenousBox.Text = "Miscallenous";
+            // 
+            // sauvegardebtnpage1
+            // 
+            this.sauvegardebtnpage1.Location = new System.Drawing.Point(609, 503);
+            this.sauvegardebtnpage1.Name = "sauvegardebtnpage1";
+            this.sauvegardebtnpage1.Size = new System.Drawing.Size(75, 23);
+            this.sauvegardebtnpage1.TabIndex = 9;
+            this.sauvegardebtnpage1.Text = "sauvegarde";
+            this.sauvegardebtnpage1.UseVisualStyleBackColor = true;
+            this.sauvegardebtnpage1.Click += new System.EventHandler(this.sauvegardebtnpage1_Click);
+            // 
+            // inputDeviceBox
+            // 
+            this.inputDeviceBox.Location = new System.Drawing.Point(59, 159);
+            this.inputDeviceBox.Name = "inputDeviceBox";
+            this.inputDeviceBox.Size = new System.Drawing.Size(206, 296);
+            this.inputDeviceBox.TabIndex = 8;
+            this.inputDeviceBox.TabStop = false;
+            this.inputDeviceBox.Text = "Input Device";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.sauvegardebtnpage2);
+            this.tabPage2.Controls.Add(this.fowardbox);
+            this.tabPage2.Controls.Add(this.reversebox);
+            this.tabPage2.Controls.Add(this.turnbox);
+            this.tabPage2.Controls.Add(this.joystick);
+            this.tabPage2.Controls.Add(this.speedbox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(834, 570);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Performance Ajustement";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // sauvegardebtnpage2
+            // 
+            this.sauvegardebtnpage2.Location = new System.Drawing.Point(689, 658);
+            this.sauvegardebtnpage2.Name = "sauvegardebtnpage2";
+            this.sauvegardebtnpage2.Size = new System.Drawing.Size(45, 23);
+            this.sauvegardebtnpage2.TabIndex = 3;
+            this.sauvegardebtnpage2.Text = "Sauvegarde";
+            this.sauvegardebtnpage2.UseVisualStyleBackColor = true;
+            // 
+            // fowardbox
+            // 
+            this.fowardbox.Location = new System.Drawing.Point(72, 182);
+            this.fowardbox.Name = "fowardbox";
+            this.fowardbox.Size = new System.Drawing.Size(597, 100);
+            this.fowardbox.TabIndex = 1;
+            this.fowardbox.TabStop = false;
+            this.fowardbox.Text = "Foward";
+            // 
+            // reversebox
+            // 
+            this.reversebox.Location = new System.Drawing.Point(72, 312);
+            this.reversebox.Name = "reversebox";
+            this.reversebox.Size = new System.Drawing.Size(597, 100);
+            this.reversebox.TabIndex = 1;
+            this.reversebox.TabStop = false;
+            this.reversebox.Text = "Reverse";
+            // 
+            // turnbox
+            // 
+            this.turnbox.Location = new System.Drawing.Point(72, 443);
+            this.turnbox.Name = "turnbox";
+            this.turnbox.Size = new System.Drawing.Size(597, 100);
+            this.turnbox.TabIndex = 1;
+            this.turnbox.TabStop = false;
+            this.turnbox.Text = "Turn";
+            // 
+            // joystick
+            // 
+            this.joystick.Location = new System.Drawing.Point(72, 581);
+            this.joystick.Name = "joystick";
+            this.joystick.Size = new System.Drawing.Size(597, 100);
+            this.joystick.TabIndex = 2;
+            this.joystick.TabStop = false;
+            this.joystick.Text = "Joystick Throw";
+            // 
+            // speedbox
+            // 
+            this.speedbox.Location = new System.Drawing.Point(72, 55);
+            this.speedbox.Name = "speedbox";
+            this.speedbox.Size = new System.Drawing.Size(597, 100);
+            this.speedbox.TabIndex = 0;
+            this.speedbox.TabStop = false;
+            // 
+            // sauvegardebtnpage2
+            // 
+            this.sauvegardebtnpage2.Location = new System.Drawing.Point(736, 499);
+            this.sauvegardebtnpage2.Name = "sauvegardebtnpage2";
+            this.sauvegardebtnpage2.Size = new System.Drawing.Size(75, 23);
+            this.sauvegardebtnpage2.TabIndex = 4;
+            this.sauvegardebtnpage2.Text = "Sauvegarde";
+            this.sauvegardebtnpage2.UseVisualStyleBackColor = true;
+            this.sauvegardebtnpage2.Click += new System.EventHandler(this.sauvegardebtnpage2_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 621);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "main";
+            this.Text = "Configuration";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox inputDeviceBox;
+        private System.Windows.Forms.Button sauvegardebtnpage1;
+        private System.Windows.Forms.GroupBox ReclineBox;
+        private System.Windows.Forms.GroupBox TiltBox;
+        private System.Windows.Forms.GroupBox ElevateBox;
+        private System.Windows.Forms.GroupBox MiscallenousBox;
+        private System.Windows.Forms.GroupBox Main_DeviceBox;
+        private System.Windows.Forms.GroupBox LegsBox;
+        private System.Windows.Forms.GroupBox ActuatorBox;
+
+        private System.Windows.Forms.GroupBox speedbox;
+        private System.Windows.Forms.GroupBox fowardbox;
+        private System.Windows.Forms.GroupBox reversebox;
+        private System.Windows.Forms.GroupBox turnbox;
+        private System.Windows.Forms.GroupBox joystick;
+        private System.Windows.Forms.Button sauvegardebtnpage2;
+
+    }
+}
---
+++ interfacetest/interfacetest/main.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interfacetest
+{
+    public partial class main : Form
+    {
+        static public List<CheckBox> DeviceCheckBoxList = new List<CheckBox>();
+        static public List<CheckBox> MainCheckBoxList = new List<CheckBox>();
+        static public List<CheckBox> TiltCheckBoxList = new List<CheckBox>();
+        static public List<CheckBox> LegsCheckBoxList = new List<CheckBox>();
+        static public List<CheckBox> RecCheckBoxList = new List<CheckBox>();
+        static public List<CheckBox> ElevateCheckBoxList = new List<CheckBox>();
+        static public List<CheckBox> ActCheckBoxList = new List<CheckBox>();
+        static public List<CheckBox> MiscCheckBoxList = new List<CheckBox>();
+        static public List<CheckBox> AllCheckBoxList = new List<CheckBox>();
+
+        static public List<TextBox> speedList = new List<TextBox>();
+        static public List<TextBox> fowardList = new List<TextBox>();
+        static public List<TextBox> reverseList = new List<TextBox>();
+        static public List<TextBox> turnList = new List<TextBox>();
+        static public List<TextBox> joystickList = new List<TextBox>();
+        static public List<TextBox> AllTextBoxList = new List<TextBox>();
+
+        public main()
+        {
+           
+            string[] device_name = { "Compact", "RM", "Mom", "ASL Digital", "Analog", "Reachtime", "Sip N Pull/Digital", "PACMS" };
+            string[] main_device_name = { "Display", "RMPJ" };
+            string[] tilt_name = { "None", "Formula", "CG", "Conventional" };
+            string[] legs_name = { "None", "Center Mount", "Dual Independent" };
+            string[] rec_name = { "None", "Formula", "Conventional" };
+            string[] elevate_name = { "None", "Beniste" };
+            string[] actuator_name = { "None", "4 Switch", "Multi-Activator","SANODE" };
+            string[] misc_name = { "ECU 1/2", "ECU 3/4", "ACC1", "ACC2", "TNC", "ASL 528", "SM 1/2", "IR Module" };
+
+            string[] speed_name = { "speed", "response" };
+            string[] foward_name = { "Foward speed", "Foward Accel", "Foward Braking" };
+            string[] reverse_name = { "Reverse speed", "Reverse Accel", "Reverse Braking" };
+            string[] turn_name = { "Turn speed", "Turn Accel", "Turn Braking" };
+            string[] joystick_name = { "Tremor Dampening", "Power Level", "G Track Enable", "Torque", "Traction" };
+           
+            InitializeComponent();
+
+            InitializeCheckBoxList(Main_DeviceBox, MainCheckBoxList, main_device_name);
+            InitializeCheckBoxList(inputDeviceBox, DeviceCheckBoxList, device_name);
+            InitializeCheckBoxList(TiltBox, TiltCheckBoxList, tilt_name);
+            InitializeCheckBoxList(LegsBox, LegsCheckBoxList, legs_name);
+            InitializeCheckBoxList(ReclineBox, RecCheckBoxList, rec_name);
+            InitializeCheckBoxList(ElevateBox, ElevateCheckBoxList, elevate_name);
+            InitializeCheckBoxList(ActuatorBox, ActCheckBoxList, actuator_name);
+            InitializeCheckBoxList(MiscallenousBox, MiscCheckBoxList, misc_name);
+
+            InitializeTextBoxList(speedbox, speedList, speed_name);
+            InitializeTextBoxList(fowardbox, fowardList, foward_name);
+            InitializeTextBoxList(reversebox, reverseList, reverse_name);
+            InitializeTextBoxList(turnbox, turnList, turn_name);
+            InitializeTextBoxList(joystick, joystickList, joystick_name);
+
+            InitializeLabel(speedbox, speedList);
+            InitializeLabel(fowardbox, fowardList);
+            InitializeLabel(reversebox, reverseList);
+            InitializeLabel(turnbox, turnList);
+            InitializeLabel(joystick, joystickList);
+        }
+
+        void InitializeCheckBoxList(GroupBox box, List<CheckBox> list, string[] listname)
+        {
+            int topPosition = 30;
+
+            foreach (string name in listname)
+            {
+                CheckBox checkBox = new CheckBox();
+
+                checkBox.Top = topPosition;
+                checkBox.Left = 10;
+                checkBox.Text = name;
+                checkBox.Name = name;
+
+                topPosition += 30;
+
+                box.Controls.Add(checkBox);
+
+                list.Add(checkBox);
+                AllCheckBoxList.Add(checkBox);
+            }
+        }
+
+        void InitializeTextBoxList(GroupBox box, List<TextBox> list, string[] listname)
+        {
+            int topPosition = 30;
+
+            foreach (string name in listname)
+            {
+                TextBox textbox = new TextBox();
+
+                textbox.Top = topPosition;
+                textbox.Left = 150;
+                textbox.Name = name;
+                textbox.Width = 40;
+
+                topPosition += 25;
+
+                box.Controls.Add(textbox);
+
+                list.Add(textbox);
+                AllTextBoxList.Add(textbox);
+            }
+        }
+        void InitializeLabel(GroupBox box, List<TextBox> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Label lbl = new Label();
+
+                lbl.Top = list[i].Top;
+                lbl.Left = Math.Abs(list[i].Left - 100);
+                lbl.Name = list[i].Name;
+                lbl.Text = list[i].Name;
+                box.Controls.Add(lbl);
+            }
+        }
+
+        private void sauvegarde_Click(object sender, EventArgs e)
+        {
+            foreach (CheckBox chk in AllCheckBoxList)
+            {
+                if(chk.Checked)
+                    MessageBox.Show(chk.Name);
+            }
+        }
+
+        private void sauvegardebtnpage1_Click(object sender, EventArgs e)
+        {
+            foreach (CheckBox chk in AllCheckBoxList)
+            {
+                if (chk.Checked)
+                    MessageBox.Show(chk.Name);
+            }
+        }
+
+        private void sauvegardebtnpage2_Click(object sender, EventArgs e)
+        {
+            int parsedValue = 0;
+            foreach (TextBox txt in AllTextBoxList)
+            {
+                if (int.TryParse(txt.Text, out parsedValue))
+                    MessageBox.Show(txt.Text);
+            }
+        }
+    }
+}
+
+
